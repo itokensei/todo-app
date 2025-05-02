efda386049b312fc3e626b39afc507ae16d5d146
kensei.ito <kensei.ito@nextbeat.net>
2025-05-02 19:03:40 +0900

    feat: add API endpoint in backend

--- app/presentation/controllers/HomeController.scala
+++ app/presentation/controllers/HomeController.scala
@@ -4,19 +4,26 @@
 
 package presentation.controllers
 
+import domain.model.task.Task
+import ixias.play.api.mvc.JsonHelper
 import play.api.mvc._
-import presentation.views.model.ViewValueHome
 import presentation.views
-import usecase.task.ShowTaskUseCase
+import presentation.views.model.ViewValueHome
+import usecase.task.{ AddTaskUseCase, ShowTaskUseCase }
 
 import javax.inject._
+import scala.concurrent.Future
 
 @Singleton
 class HomeController @Inject() (
-  val controllerComponents: ControllerComponents,
-  showTaskUseCase:          ShowTaskUseCase,
-) extends BaseController {
-  def index() = Action.async {
+  implicit
+  val mcc:             MessagesControllerComponents,
+  showTaskUseCase:     ShowTaskUseCase,
+  addTaskUseCase:      AddTaskUseCase
+) extends MessagesAbstractController(mcc) {
+  implicit val ec: scala.concurrent.ExecutionContext = mcc.executionContext
+
+  def index() = Action.async { implicit request: MessagesRequest[AnyContent] =>
     val vv = ViewValueHome(
       title  = "Home",
       cssSrc = Seq("main.css"),
@@ -25,5 +32,13 @@ class HomeController @Inject() (
 
     val future = showTaskUseCase.execute()
     future.map(allTasks => Ok(views.html.Task(vv, allTasks)))(controllerComponents.executionContext)
+  }
+
+  def add() = Action.async { implicit request =>
+    JsonHelper.bindFromRequest[AddTaskRequest].fold(
+      formWithErrors => Future(formWithErrors),
+      addTaskRequest =>
+        addTaskUseCase.execute(Task(addTaskRequest)).map(_ => Redirect(routes.HomeController.index()))
+    )
   }
 }
