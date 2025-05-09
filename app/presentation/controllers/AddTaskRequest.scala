92ec1b8c26d71b3bb9f196f8d63f21a8eb3d5eb8
kensei.ito <kensei.ito@nextbeat.net>
2025-05-09 16:27:58 +0900

    refactor/remove unnecessary comments

--- app/presentation/controllers/AddTaskRequest.scala
+++ app/presentation/controllers/AddTaskRequest.scala
@@ -3,7 +3,7 @@ package presentation.controllers
 import domain.model.task.Category
 import play.api.libs.json.{ Json, Reads }
 
-case class AddTaskRequest(title: String, body: String, categoryId: Option[Category.Id]) // TODO
+case class AddTaskRequest(title: String, body: String, categoryId: Option[Category.Id])
 
 object AddTaskRequest {
   implicit val reads: Reads[AddTaskRequest] = Json.reads[AddTaskRequest]
