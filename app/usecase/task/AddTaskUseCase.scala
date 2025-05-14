35624efeb7c7b85a04d47869c2f9e476c302d4bd
kensei.ito <kensei.ito@nextbeat.net>
2025-05-14 14:12:19 +0900

    refactor/rename TaskElement and ShowTaskUseCaseDto to TaskItem/Dto

--- app/usecase/task/AddTaskUseCase.scala
+++ app/usecase/task/AddTaskUseCase.scala
@@ -13,7 +13,7 @@ class AddTaskUseCase @Inject() (
   taskQueryService:              TaskQueryService,
   implicit val executionContext: ExecutionContext
 ) {
-  def execute(addTaskRequest: AddTaskRequest): Future[ShowTaskUseCaseDto] = {
+  def execute(addTaskRequest: AddTaskRequest): Future[TaskItemDto] = {
     def taskWithNoId   = Task(
       id         = None,
       title      = addTaskRequest.title,
@@ -26,7 +26,7 @@ class AddTaskUseCase @Inject() (
     for {
       _        <- execution
       category <- categoryFuture
-    } yield ShowTaskUseCaseDto(
+    } yield TaskItemDto(
       title         = task.title,
       body          = task.body,
       state         = task.state,
