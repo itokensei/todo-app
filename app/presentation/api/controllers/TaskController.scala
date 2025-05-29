1a3d29dbaac795f6e208dcf6c4bbeaf45cf4eaa7
kensei.ito <kensei.ito@nextbeat.net>
2025-05-29 15:58:04 +0900

    feat/implement list() as add todo api

--- app/presentation/api/controllers/TaskController.scala
+++ app/presentation/api/controllers/TaskController.scala
@@ -4,7 +4,7 @@ import ixias.play.api.mvc.JsonHelper
 import play.api.libs.json.Json
 import play.api.mvc._
 import presentation.{ AddTaskRequest, DeleteTaskRequest, UpdateTaskRequest }
-import usecase.task.{ AddTaskUseCase, DeleteTaskUseCase, UpdateTaskUseCase }
+import usecase.task._
 
 import javax.inject._
 import scala.concurrent.Future
@@ -12,11 +12,23 @@ import scala.concurrent.Future
 @Singleton
 class TaskController @Inject() (
   val controllerComponents: ControllerComponents,
+  showTaskUseCase:          ShowTaskUseCase,
+  showCategoryUseCase:      ShowCategoryUseCase,
   addTaskUseCase:           AddTaskUseCase,
   updateTaskUseCase:        UpdateTaskUseCase,
   deleteTaskUseCase:        DeleteTaskUseCase
 ) extends BaseController {
   implicit val ec: scala.concurrent.ExecutionContext = controllerComponents.executionContext
+
+  def list() = Action.async { implicit request =>
+    val allTasksFuture      = showTaskUseCase.execute()
+    val allCategoriesFuture = showCategoryUseCase.execute()
+    val allStatus           = domain.model.task.Status.values
+    for {
+      allTasks      <- allTasksFuture
+      allCategories <- allCategoriesFuture
+    } yield Ok(Json.toJson(ShowTaskUseCaseDto(allTasks, allStatus, allCategories)))
+  }
 
   def add() = Action.async { implicit request =>
     JsonHelper.bindFromRequest[AddTaskRequest].fold(
